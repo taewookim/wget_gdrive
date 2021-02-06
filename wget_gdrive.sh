dbdfc72796175b687420bb8c0384560fb80c8138
R. Rajesh Jeba Anbiah <rrjanbiah@users.noreply.github.com>
2021-02-07 02:27:59 +0530

    download resume support with --continue switch

--- wget_gdrive.sh
+++ wget_gdrive.sh
@@ -7,4 +7,4 @@ fi
 
 confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='$1 -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
 echo $confirm
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$confirm&id=$1" -O $2 && rm -rf /tmp/cookies.txt
+wget --continue --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$confirm&id=$1" -O $2 && rm -rf /tmp/cookies.txt
